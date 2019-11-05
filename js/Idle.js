49117fa49b2b2ae2390850b77501d20ea72a635d
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2019-11-04 23:33:30 -0800

    .

--- js/Idle.js
+++ js/Idle.js
@@ -13,7 +13,6 @@ var partsprogress=0
 var canparts=0
 ////////////////////////////////////////////////////////////////////////////
 
-
 ////////////////////////////////////////////////////////////////////////////
 
 
@@ -27,8 +26,8 @@ if(localStorage.getItem('Idle.Game') == null){
 
 //--Dynamically creates buildings--//
 function InitBuildings(){//name,cost,persec
-	LoadBuilding("Sieve",10,1);
-	LoadBuilding("Mine",100,5);
+	LoadBuilding("Employee",10,0.2);
+	LoadBuilding("Robot",100,5);
 }
 function LoadBuilding(name,cost,persec){
 	var id = buildings.length;
@@ -53,22 +52,33 @@ function InitData(){
 	BCost[id]=round((BCost[id]*((1.3**Game.BQty[id]))*BCostMul),0);
 	}
 	document.getElementById("parts").innerHTML = Game.parts;
-	document.getElementById("Building1Qty").innerHTML = "Sieve: " + Game.BQty[0];
+	document.getElementById("Building1Qty").innerHTML = buildings[0].Name +": "+ Game.BQty[0];
 	document.getElementById("Building1Cost").innerHTML = "Cost: " + BCost[0];
-	document.getElementById("Building2Qty").innerHTML = "Mine: " + Game.BQty[1];
+	document.getElementById("Building1PerSec").innerHTML = "Parts/sec " + buildings[0].PerSec;
+	document.getElementById("Building2Qty").innerHTML = buildings[1].Name +": "+ Game.BQty[1];
 	document.getElementById("Building2Cost").innerHTML = "Cost: " + BCost[1];
-
+	document.getElementById("Building2PerSec").innerHTML = "Parts/sec " + buildings[1].PerSec;
+	document.getElementById("money").innerHTML = Game.money;
+	UpdateTick();
 }
 
 
 //--Updates visuals for numbers at call--//
 function UpdateData(){
 	document.getElementById("parts").innerHTML = Game.parts;
-	document.getElementById("Building1Qty").innerHTML = "Sieve: " + Game.BQty[0];
+	document.getElementById("Building1Qty").innerHTML = buildings[0].Name +": "+ Game.BQty[0];
 	document.getElementById("Building1Cost").innerHTML = "Cost: " + BCost[0];
-	document.getElementById("Building2Qty").innerHTML = "Mine: " + Game.BQty[1];
+	document.getElementById("Building1PerSec").innerHTML = "Parts/sec " + buildings[0].PerSec;
+	document.getElementById("Building2Qty").innerHTML = buildings[1].Name +": "+ Game.BQty[1];
 	document.getElementById("Building2Cost").innerHTML = "Cost: " + BCost[1];
+	document.getElementById("Building2PerSec").innerHTML = "Parts/sec " + buildings[1].PerSec;
+	document.getElementById("money").innerHTML = Game.money;
 }
+function UpdateParts(){
+	document.getElementById("parts").innerHTML = Game.parts;
+	document.getElementById("money").innerHTML = Game.money;
+}
+
 
 
 //--Parents--//
@@ -78,6 +88,7 @@ function GameData(){
 	this.parts = 0;
 	this.BQty=[]
 	this.Upgrades=[];
+	this.money=0;
 }
 function Building() {
 	this.Name = "Name";
@@ -90,6 +101,9 @@ function Building() {
 function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
+function rounddown(value, decimals) {
+  return Number(Math.floor(value+'e'+decimals)+'e-'+decimals);
+}
 
 
 //--Upgrades--//
@@ -98,11 +112,18 @@ function BuyUpgrade(id){
 	InitData();
 }
 
+//--Selling parts--//
+function sellparts(){
+	Game.money+=rounddown(Game.parts,0);
+	Game.parts=0;
+	UpdateData();
+}
+
 
 //--Build and Validity Check--//
 function Build(id){
-	if (Game.parts >= BCost[id]){
-		Game.parts -= BCost[id];
+	if (Game.money >= BCost[id]){
+		Game.money -= BCost[id];
 		Game.BQty[id] = Game.BQty[id]+1;
 		BCost[id]=round((BCostBase[id]*((1.3**Game.BQty[id]))*BCostMul),0);
 		UpdateData();
@@ -158,19 +179,24 @@ function ManualSave(){
 
 //Global tick timer
 var Timerparts = window.setInterval(function(){partsTick()}, 1000);
-var TimerUpdate = window.setInterval(function(){UpdateTick()}, 250);
+var TimerUpdate = window.setInterval(function(){UpdateTick()}, 1000);
+var Timermoney = window.setInterval(function(){moneyTick()},1000);
 function partsTick(){
 	for (var MT = 0;MT < buildings.length;MT++) {
 		Game.parts += Game.BQty[MT]* buildings[MT].PerSec;
+		Game.parts=round(Game.parts,1);//prevents "drift" of parts due to floating point. Alternative is Game.parts.toFixed(1) on following line
 		document.getElementById("parts").innerHTML = Game.parts;
 	}
 }
 function UpdateTick(){
 	for(var UT = 0;UT < buildings.length;UT++){
-		partspersec = partspersec+(Game.BQty[UT]*buildings[UT].PerSec);
+		partspersec = partspersec+(Game.BQty[UT]*buildings[UT].PerSec);//multiplies all buildings by their quatites and then adds them all together
 	}
 	document.getElementById("partspersec").innerHTML = partspersec;
-	partspersec = 0;
+	partspersec = 0;//reset to zero is required or it will loop on itself
+}
+function moneyTick(){
+	document.getElementById("money").innerHTML = Game.money;
 }
 
 
@@ -192,6 +218,6 @@ function Gatherparts(){
 				canparts=0;
 //				document.getElementById("moneybutton").disabled=false;
 			};
-		}, 10);
+		}, 20);
 	};
 };
